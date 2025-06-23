b73aab482832b092b42543e2f71aea084a08b2d7
Yash Bansal <129962666+YashXBansal@users.noreply.github.com>
2025-06-23 08:08:25 +0530

    Update page.tsx

--- app/(root)/experience/[expId]/page.tsx
+++ app/(root)/experience/[expId]/page.tsx
@@ -79,7 +79,7 @@ export default function Experience({ params }: ExperiencePageProps) {
             />
 
             <div className="flex-1 text-left leading-tight">
-              <p className="font-medium">{"Naman Barkiya"}</p>
+              <p className="font-medium">{"Yash Bansal"}</p>
               <p className="text-[12px] text-muted-foreground">
                 @{siteConfig.username}
               </p>
